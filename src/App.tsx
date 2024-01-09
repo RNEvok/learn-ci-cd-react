9248e8f3bae3b57645d872aaadfd365c9e59bf46
RNEvok <81921589+RNEvok@users.noreply.github.com>
2024-01-10 01:11:23 +0300

    1.0.1

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React v1.0.0
+          Learn React v1.0.1
         </a>
       </header>
     </div>
