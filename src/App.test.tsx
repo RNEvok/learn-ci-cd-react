28942646f15bf240946f79289d0b7c5909658338
RNEvok <81921589+RNEvok@users.noreply.github.com>
2024-01-10 01:14:52 +0300

    1.0.2

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react v1.0.1/i);
+  const linkElement = screen.getByText(/learn react v1.0.2/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React v1.0.1
+          Learn React v1.0.2
         </a>
       </header>
     </div>
